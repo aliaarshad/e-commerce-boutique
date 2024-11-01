ad265639a587397cc58606c3f8bbeabc57984c09
Alia <alia.bsse.s.1@gmail.com>
2024-11-01 22:28:40 +0500

    update trending section images links

--- src/components/TrendingSection.jsx
+++ src/components/TrendingSection.jsx
@@ -1,20 +1,29 @@
 import './TrendingSection.css';
+import ht1 from '../assets/img/trend/ht-1.jpg';
+import ht2 from '../assets/img/trend/ht-2.jpg';
+import ht3 from '../assets/img/trend/ht-3.jpg';
+import bs1 from '../assets/img/trend/bs-1.jpg';
+import bs2 from '../assets/img/trend/bs-2.jpg';
+import bs3 from '../assets/img/trend/bs-3.jpg';
+import f1 from '../assets/img/trend/f-1.jpg';
+import f2 from '../assets/img/trend/f-2.jpg'; 
+import f3 from '../assets/img/trend/f-3.jpg';
 
 const products = {
   hotTrend: [
-    { id: 1, name: 'Red Dress', image: 'src/assets/img/trend/ht-1.jpg', rating: 4.5, price: '$120' },
-    { id: 2, name: 'Summer Hat', image: 'src/assets/img/trend/ht-2.jpg', rating: 4, price: '$35' },
-    { id: 3, name: 'Blue Shirt', image: 'src/assets/img/trend/ht-3.jpg', rating: 4.3, price: '$45' },
+    { id: 1, name: 'Red Dress', image: ht1, rating: 4.5, price: '$120' },
+    { id: 2, name: 'Summer Hat', image: ht2, rating: 4, price: '$35' },
+    { id: 3, name: 'Blue Shirt', image: ht3, rating: 4.3, price: '$45' },
   ],
   bestSeller: [
-    { id: 4, name: 'Leather Shoes', image: 'src/assets/img/trend/bs-1.jpg', rating: 4.8, price: '$90' },
-    { id: 5, name: 'Classic Watch', image: 'src/assets/img/trend/bs-2.jpg', rating: 4.9, price: '$150' },
-    { id: 6, name: 'Elegant Ring', image: 'src/assets/img/trend/bs-3.jpg', rating: 4.7, price: '$200' },
+    { id: 4, name: 'Leather Shoes', image: bs1, rating: 4.8, price: '$90' },
+    { id: 5, name: 'Classic Watch', image: bs2, rating: 4.9, price: '$150' },
+    { id: 6, name: 'Elegant Ring', image: bs3, rating: 4.7, price: '$200' },
   ],
   feature: [
-    { id: 7, name: 'Designer Bag', image: 'src/assets/img/trend/f-1.jpg', rating: 4.6, price: '$250' },
-    { id: 8, name: 'Silk Scarf', image: 'src/assets/img/trend/f-2.jpg', rating: 4.4, price: '$60' },
-    { id: 9, name: 'Sunglasses', image: 'src/assets/img/trend/f-3.jpg', rating: 4.3, price: '$75' },
+    { id: 7, name: 'Designer Bag', image: f1, rating: 4.6, price: '$250' },
+    { id: 8, name: 'Silk Scarf', image: f2, rating: 4.4, price: '$60' },
+    { id: 9, name: 'Sunglasses', image: f3, rating: 4.3, price: '$75' },
   ],
 };
 
